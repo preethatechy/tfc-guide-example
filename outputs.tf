4ba822599a9196b63d85e6f06d2f5192a3d96154
preethatechy <128165553+preethatechy@users.noreply.github.com>
2025-01-07 13:24:35 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,5 @@ output "instance_arn" {
   value = aws_instance.ubuntu.arn
 }
 
+
+def checkAppInfraDeploy(config) { def isAppDeploy = !readConfigFile().component.releaseComponents.isEmpty() return config.executeCreateInfrastructure && !config.executeDestroyInfrastructure ? (isAppDeploy ? pipelineConstants.stages.CREATE_INFRA_AND_APP_DEPLOY : pipelineConstants.stages.CREATE_INFRA) : config.executeDestroyInfrastructure && !config.executeCreateInfrastructure ? pipelineConstants.stages.DESTROY_INFRA : config.executeCreateInfrastructure && config.executeDestroyInfrastructure ? (isAppDeploy ? pipelineConstants.stages.CREATE_AND_DESTROY_INFRA_AND_APP_DEPLOY : pipelineConstants.stages.CREATE_AND_DESTROY_INFRA) : null }
